2de24190f9a7e5dbda360059b3af513deb13d8a3
Musab Momin <mominmusab127@gmail.com>
2024-07-23 23:03:51 +0530

    feat: primitive data types

---
+++ primitive-data-types/data-type.java
@@ -0,0 +1,27 @@
+class DataTypes {
+  public static void main(String args[]) {
+
+    // Integer type
+    byte b = 126; // 1 byte
+    System.out.println(b);
+    short s = 32766; // 8 bytes
+    System.out.println(s);
+    int i = 2147483646;
+    System.out.println(i);
+    long l = 9223372036854775806l;
+    System.out.println(l);
+
+    // Float type
+    double d = 5.6; // 8 bytes
+    System.out.println(d);
+    float f = 69.58f; // 4 bytes
+    System.out.println(f);
+
+    // Character
+    char c = 'k'; // 2 bytes
+    System.out.println(c);
+    // Boolean
+    boolean bi = true;
+    System.out.println(bi);
+  }
+}
