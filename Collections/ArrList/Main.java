f9ce631068de84761f2a6381f73bca5c5e40c58b
Musab Momin <mominmusab127@gmail.com>
2024-08-04 22:04:24 +0530

    collections framework wp

---
+++ Collections/ArrList/Main.java
@@ -0,0 +1,20 @@
+package Collections.ArrList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<Integer>();
+        arr.add(59);
+        arr.add(9);
+        arr.add(4);
+        arr.add(2);
+        arr.add(12);
+
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+    }
+}
